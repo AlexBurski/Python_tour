ac7248987261af283b3455c2f8380c0e2e892fb3
AlexBurski <bssb.sbbs@mail.ru>
2021-11-02 22:09:59 +0300

    functional prog tasks

---
+++ functional_prog_task/decoreter_time.py
@@ -0,0 +1,20 @@
+import time
+
+
+def decorater(func):
+    def inside(*args: tuple):
+        start = time.time()
+        func(*args)
+        end = time.time()
+        print(f'Function {func}: finished in {end - start}')
+        return func(*args)
+    return inside
+
+
+@decorater
+def math(a: int, b: int):
+    return a ** b ** 5
+
+
+
+math(9, 9)
